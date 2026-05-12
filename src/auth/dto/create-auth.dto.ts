39eeba5471fafad2e9aaf279936fa19df0c2a84c
ArielQ1 <aquizaya1@umsa.bo>
2026-05-12 10:21:54 -0400

    fixed errors

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,12 +1,36 @@
-import type { RolUsuario } from '../../generated/prisma/enums';
+import { IsString, IsNotEmpty, IsOptional, IsEnum } from 'class-validator';
+import { RolUsuario } from '../../generated/prisma/enums';
 
 export class CreateAuthDto {
+  @IsString()
+  @IsNotEmpty()
   nombres!: string;
+
+  @IsString()
+  @IsNotEmpty()
   apellidos!: string;
+
+  @IsString()
+  @IsNotEmpty()
   carnet!: string;
+
+  @IsString()
+  @IsNotEmpty()
   password!: string;
+
+  @IsEnum(RolUsuario)
+  @IsNotEmpty()
   rol!: RolUsuario;
+
+  @IsString()
+  @IsOptional()
   username?: string;
+
+  @IsString()
+  @IsOptional()
   correo?: string;
+
+  @IsString()
+  @IsOptional()
   celular?: string;
 }
